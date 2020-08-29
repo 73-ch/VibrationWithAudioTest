2fb9cbbcc759125291a3202eb1f9008cde0ffa61
634nami <cigarbox.nami@gmail.com>
2020-08-29 20:10:10 +0900

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button id="start" @click="start">start</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {Filter, Player, FFT} from "tone";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    start() {
+      const player = new Player("./test40.wav");
+      // play as soon as the buffer is loaded
+      player.autostart = true;
+      player.loop = true;
+
+      const boundary_freq = 80;
+      console.log(player.context);
+
+      const audio_filter = new Filter(boundary_freq, 'highpass', -96);
+      const vibration_filter = new Filter(boundary_freq, 'lowpass', -96);
+
+      player.connect(audio_filter);
+      player.connect(vibration_filter);
+
+      audio_filter.toDestination();
+
+      const fft = new FFT();
+      vibration_filter.connect(fft);
+      // let before = new Float32Array(1024);
+
+      let beforeAt = 0;
+
+      setInterval(() => {
+        // console.log(fft.getValue()[0]);
+
+        if (fft.getValue()[0] > -40  && player.context.currentTime - beforeAt > 0.2) {
+          beforeAt = player.context.currentTime;
+          console.log("bang");
+        }
+      },10);
+    },
   }
 }
 </script>
